0164555c8bfb150db21fdae201304db9085f9356
Pradnesh Kore <pradnesh.kore@gmail.com>
2023-08-13 17:00:59 +0530

    Added calculation mode

--- app/src/main/java/com/pradyk/speedcalculator/model/Calculator.kt
+++ app/src/main/java/com/pradyk/speedcalculator/model/Calculator.kt
@@ -2,9 +2,24 @@ package com.pradyk.speedcalculator.model
 
 object Calculator {
 
-    fun getSpeed(distance: Double, time: Double) = distance/time
+    enum class Mode {
+        GET_SPEED, GET_TIME, GET_DISTANCE
+    }
 
-    fun getTime(distance: Double, speed: Double) = distance/speed
+    private fun getSpeed(distance: Double, time: Double) = distance / time
 
-    fun getDistance(speed: Double, time: Double) = speed * time
+    private fun getTime(distance: Double, speed: Double) = distance / speed
+
+    private fun getDistance(speed: Double, time: Double) = speed * time
+
+    fun calculate(
+        mode: Mode,
+        distance: Double = 0.0,
+        speed: Double = 0.0,
+        time: Double = 0.0
+    ) = when (mode) {
+        Mode.GET_SPEED -> getSpeed(distance, time)
+        Mode.GET_TIME -> getTime(distance, speed)
+        Mode.GET_DISTANCE -> getDistance(speed, time)
+    }
 }
